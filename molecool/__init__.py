59ea2378ab32b770fc5e266ab6e9c7dad46bc0b5
Yong Zhang <yzhang19@nd.edu>
2020-12-15 15:00:11 -0500

    notebook function updated

--- molecool/__init__.py
+++ molecool/__init__.py
@@ -4,7 +4,7 @@ A Python package for analyzing and visualizing xyz files.
 """
 
 # Add imports here
-from .functions import canvas
+from .functions import canvas,calculate_distance,open_pdb,open_xyz,write_xyz,draw_molecule,calculate_angle,bond_histogram,build_bond_list
 
 # Handle versioneer
 from ._version import get_versions
