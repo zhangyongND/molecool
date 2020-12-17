1744557f41b10bbccbd6ef79c91d588652692172
Yong Zhang <yzhang19@nd.edu>
2020-12-17 14:22:04 -0500

    fix imports on modules

--- molecool/functions.py
+++ molecool/functions.py
@@ -7,11 +7,6 @@ Handles the primary functions
 source code comes here
 """
 
-import os
-import numpy as np
-import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
-
 def canvas(with_attribution=True):
     """
     Placeholder function to show example docstring (NumPy format)
--- molecool/visualize.py
+++ molecool/visualize.py
@@ -5,7 +5,7 @@ Visualization functions
 import numpy as np
 import matplotlib.pyplot as plt
 
-from mpl_toolkits.mplot3d import Axes3D
+from mpl_toolkits.mplot3d import Axes3D # noqa: F401
 
 from .atom_data import atom_colors
 
